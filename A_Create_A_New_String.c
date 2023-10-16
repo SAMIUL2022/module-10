89f044f6ebcb09949c4f286d5ed64762444279b2
SAMIUL2022 <sami35bd@gmail.com>
2023-10-16 18:27:30 +0600

    first commit

---
+++ A_Create_A_New_String.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    char S[1001],t[1001];
+    scanf("%s %s",S,t);
+    int a=strlen(S);
+    int b=strlen(t);
+
+    printf("%d %d\n",a,b);
+    printf("%s %s\n",S,t);
+
+
+
+
+     
+    return 0;
+}
---
+++ string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+
+char a [6]="shamim";
+
+printf("%s\n",a);
+
+     
+    return 0;
+}
